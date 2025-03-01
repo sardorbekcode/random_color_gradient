489ce94342d5eb0b0e970e8178a6203c762d0c40
sardorbekcode <sardorbekmusurmonov0@gmail.com>
2025-03-01 08:06:07 +0500

    javascript qaytadan yozib chiqildi

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-const boy = document.querySelector('body');
+const body = document.querySelector('body');
 const container = document.querySelector('.container');
 const colorText = document.querySelector('.color-text');
 
-const values = [
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f'
-]
+const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
 
 // Function to generate random hex color
 
@@ -27,10 +10,32 @@ function getGradient() {
     let color = '#';
     for (let i = 0; i < 6; i++) {
         const randomNumber = Math.trunc(Math.random() * values.length);
-        console.log(randomNumber);
-        
+        color += values[randomNumber]
     }
+    return color
         
 }
 
-getGradient();
+
+
+
+// set color to body
+
+function setGradient() {
+    const color1 = getGradient()
+    const color2 = getGradient()
+    const randomDeg = Math.trunc(Math.random() * 360)
+    const bgColor = `linear-gradient(
+        ${randomDeg}deg,
+        ${color1},
+        ${color2}
+    )`
+
+    body.style.background = bgColor
+    colorText.textContent = bgColor
+    
+}
+
+setGradient()
+
+container.addEventListener("click", setGradient)
